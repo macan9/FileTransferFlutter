db2e456d24f3cdb5dbe4d0713380a86df0ba4e45
macan9 <729672251@qq.com>
2026-04-27 15:47:45 +0800

    文件名更换

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract final class AppConstants {
-  static const appName = '\u6587\u4ef6\u4f20\u8f93\u52a9\u624b';
+  static const appName = '\u5c0f\u9a6c\u5de5\u5177\u7bb1';
   static const appSlogan =
       '\u672c\u5730\u4f18\u5148\u7684\u6587\u4ef6\u540c\u6b65\u4e0e\u5c40\u57df\u7f51\u4f20\u8f93\u4e2d\u5fc3';
   static const downloadFolderName = 'FileTransferDownloads';
